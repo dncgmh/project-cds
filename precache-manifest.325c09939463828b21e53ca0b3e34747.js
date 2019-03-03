eca8f721fec577d78709c36615e9b77ebdefc6ea
Doan Cong Minh <dncgmh@gmail.com>
2019-03-03 08:22:43 +0700

    Updates

---
+++ precache-manifest.325c09939463828b21e53ca0b3e34747.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "3fb8dbe704d1bb910b6f",
+    "url": "/static/js/main.3fb8dbe7.chunk.js"
+  },
+  {
+    "revision": "d5846844e4ef858e214d",
+    "url": "/static/js/2.d5846844.chunk.js"
+  },
+  {
+    "revision": "3fb8dbe704d1bb910b6f",
+    "url": "/static/css/main.a745bf15.chunk.css"
+  },
+  {
+    "revision": "d5846844e4ef858e214d",
+    "url": "/static/css/2.8ccb98be.chunk.css"
+  },
+  {
+    "revision": "753cc9c8a23385c781cd2510027345f8",
+    "url": "/index.html"
+  }
+];
